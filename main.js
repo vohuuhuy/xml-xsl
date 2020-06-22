c9c6cfbe4b7f5f02d535e4fb8a531226ed4a990b
vohuy <vhhuy.c3tqcap.a7@gmail.com>
2020-06-22 16:43:39 +0700

    update xml

--- main.js
+++
@@ -1,7 +0,0 @@
-var xml = new ActiveXObject("Microsoft.XMLDOM")
-xml.async = false
-xml.load("QLTH.xml")
-var xsl = new ActiveXObject("Microsoft.XMLDOM")
-xsl.async = false
-xsl.load("QLTH.xsl")
-document.write(xml.transformNode(xsl))
